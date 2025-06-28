9d48f476dd05e09d1bb7f3be14ce72679ce18d20
Rafa-X <RD.2425@protonmail.com>
2025-06-27 22:11:24 -0400

    v3

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,14 +1,56 @@
-const btnSearch = document.getElementById('btnSearch');
-const btnReset = document.getElementById('btnReset');
-const destination = document.getElementById('destination');
+const btnSearch =   document.getElementById('btnSearch');
+const btnReset =    document.getElementById('btnReset');
+const destinationInput = document.getElementById('destination');
+const destinations = [];
+const resultsDiv =  document.getElementById('results');
 
 function searchDestination(){
-    const dest = destination.value;
-    console.log("dest:", dest)
+    const destination = destinationInput.value;
+    console.log("dest:", destination)
+    if (destination.trim() === "") {return;}
+
+    fetch('travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+        data.countries.forEach(country => {
+            country.cities.forEach(city => {
+                if (city.description.toLowerCase().includes( destination.toLowerCase() )){
+                    destinations.push({ 
+                        image: city.imageUrl,
+                        name: city.name, 
+                        description: city.description
+                    })
+                    console.log(city.name)
+                }
+            })
+        })
+
+        resultsDiv.innerHTML = "";
+        if (destinations.length === 0) {
+            resultsDiv.innerHTML = "<p>No matches found.</p>";
+        }else{
+            destinations.forEach(item => {
+                const p = document.createElement('p');
+                p.innerHTML = 
+                    `<img src="${item.image}" alt="${item.name}" /><br>
+                    <strong>${item.name}</strong><br>
+                    <small>${item.description}</small>
+                    <button>Visit</button>`;
+                resultsDiv.appendChild(p);
+            })
+            resultsDiv.style.display = "block";
+        }
+    })
+    .catch(error => {
+        console.log("Error:", error);
+    })
 }
 
 function resetDestination(){
     destination.value = '';
+    resultsDiv.innerHTML = '';
+    resultsDiv.style.display = "none"; // Lo oculta visualmente
+    destinations = [];
 }
 
 btnSearch.addEventListener('click', searchDestination);
