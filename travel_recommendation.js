25a703363de778a837a58bbbe61007897a8b308a
Rafa-X <RD.2425@protonmail.com>
2025-06-30 22:22:05 -0400

    v3

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -11,24 +11,55 @@ function searchDestination(){
 
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
-    .then(data => {
+    .then(data => {    
+        const regex = new RegExp(destination.toLowerCase(), "i"); // "i" = case insensitive
+        
         data.countries.forEach(country => {
             country.cities.forEach(city => {
-                if (city.description.toLowerCase().includes( destination.toLowerCase() )){
-                    destinations.push({ 
+                if (regex.test(city.description)) {
+                    destinations.push({
                         image: city.imageUrl,
-                        name: city.name, 
+                        name: city.name,
                         description: city.description
-                    })
+                    });
                     console.log(city.name)
                 }
+            });
+        });
+
+        Object.entries(data).forEach(([key, items]) => {
+            items.forEach(item => {
+                //contries key value that have CITIES
+                if (Array.isArray(item.cities)) {
+                    item.cities.forEach(city => {
+                        if( regex.test(city.description.toLowerCase()) ){
+                            destinations.push({
+                                image: city.imageUrl,
+                                name: city.name,
+                                description: city.description,
+                            });
+                            console.log(city.name)
+                        }
+                    });
+                }else{
+                    //Places key value that don't have CITIES
+                    if( regex.test(item.description.toLowerCase()) ){
+                        destinations.push({
+                            image: item.imageUrl,
+                            name: item.name,
+                            description: item.description,
+                        });
+                        console.log(item.name)
+                    }
+                }
             })
-        })
+        });
 
         resultsDiv.innerHTML = "";
         if (destinations.length === 0) {
             resultsDiv.innerHTML = "<p>No matches found.</p>";
         }else{
+            console.log(destinations.length)
             destinations.forEach(item => {
                 const p = document.createElement('p');
                 p.innerHTML = 
